94b384812916dccb6e6c2a1983401024e6df084b
slimmsyd <84546691+slimmsyd@users.noreply.github.com>
2021-08-19 18:09:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-// Create TIMELIME
+
+// Getting Elements and Classes 
 
 
-let tl = gsap.timeline({ defaults: {ease: 'power1.out'}});
-
-tl.to(".text" ,{y:'5%', duration: 1, stagger: .5});
-tl.to(".intro", { opacity: 0, duration: 1})
-tl.to(".intro", {y: "100%"})
-tl.from("#line", {duration:1,x:-200, opacity: 0 })
-tl.from("#linedown", {duration: 1,  opacity: 0})
+var x = document.querySelector(".menu");
+var y = document.querySelector(".header")
 
 
-let ntl = new TimelineLite();
-ntl.to(".word-section", {opacity:1, duration:5, })
 
-const controller = new ScrollMagic.Controller();
 
-const scene = new ScrollMagic.Scene({
-    triggerElement: '.word-section',
-    triggerHook: "onEnter",
 
+x.addEventListener("click", () => {
+    if (x.style.display !== "block") {
+        x.style.display ="none"
+        y.style.display = "block"
+      console.log("fjlsfj")
+    }
 })
-.setPin(".section2")
-.setTween(ntl)
-.addTo(controller)
-
-
-
